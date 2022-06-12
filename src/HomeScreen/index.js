d153232140590de2d45c260e29eb2ae85fb54767
rmartey <marteyrichmond001@gmail.com>
2022-06-12 08:48:06 +0000

    designed home screen

--- src/HomeScreen/index.js
+++ src/HomeScreen/index.js
@@ -1,10 +1,89 @@
-import { View, Text } from "react-native";
+import {
+	View,
+	Text,
+	TouchableOpacity,
+	ImageBackground,
+	ScrollView,
+} from "react-native";
 import React from "react";
+import styles from "./styles";
+
+import { Entypo } from "@expo/vector-icons";
+
+//images
+import laundaryimage1 from "../../assets/laundary-image1.jpg";
+import cleaningimage from "../../assets/cleaningImage.jpg";
 
 const HomeScreen = () => {
 	return (
-		<View>
-			<Text>HomeScreen</Text>
+		<View style={styles.root}>
+			<View style={styles.locationContainer}>
+				<Entypo name="location-pin" size={24} color="#1779d4" />
+				<Text style={styles.locationText}>Assin Fosu</Text>
+			</View>
+			<Text style={styles.serivicesText}>Services</Text>
+			<ScrollView showsVerticalScrollIndicator={false}>
+				<TouchableOpacity>
+					<View style={styles.servicesContainer}>
+						<ImageBackground
+							source={laundaryimage1}
+							resizeMode="cover"
+							style={styles.servicesImage}
+							imageStyle={{ borderRadius: 25 }}
+						>
+							<Text
+								style={[
+									styles.serivicesText,
+									{ marginLeft: 20, marginTop: 20 },
+								]}
+							>
+								Laundary
+							</Text>
+							<Text style={{ color: "yellow", marginLeft: 20 }}>
+								{"Dress & shine"}
+							</Text>
+							<View style={styles.minPriceContainer}>
+								<Text style={{ color: "yellow", fontSize: 18, opacity: 0.9 }}>
+									GHS 12.00
+								</Text>
+								<Text style={{ color: "yellow", fontSize: 16, opacity: 0.6 }}>
+									min.
+								</Text>
+							</View>
+						</ImageBackground>
+					</View>
+				</TouchableOpacity>
+				<TouchableOpacity>
+					<View style={styles.servicesContainer}>
+						<ImageBackground
+							source={cleaningimage}
+							resizeMode="cover"
+							style={styles.servicesImage}
+							imageStyle={{ borderRadius: 25 }}
+						>
+							<Text
+								style={[
+									styles.serivicesText,
+									{ marginLeft: 20, marginTop: 20 },
+								]}
+							>
+								Cleaning
+							</Text>
+							<Text style={{ color: "yellow", marginLeft: 20 }}>
+								{"Dress & shine"}
+							</Text>
+							<View style={styles.minPriceContainer}>
+								<Text style={{ color: "yellow", fontSize: 18, opacity: 0.9 }}>
+									GHS 6.00
+								</Text>
+								<Text style={{ color: "yellow", fontSize: 16, opacity: 0.6 }}>
+									min.
+								</Text>
+							</View>
+						</ImageBackground>
+					</View>
+				</TouchableOpacity>
+			</ScrollView>
 		</View>
 	);
 };
--- src/HomeScreen/styles.js
+++ src/HomeScreen/styles.js
@@ -1,5 +1,54 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	root: {
+		marginTop: 50,
+		padding: 20,
+		flex: 1,
+		backgroundColor: "#ffffff",
+	},
+	locationContainer: {
+		flexDirection: "row",
+		widthL: "100%",
+		alignItems: "center",
+	},
+	locationText: {
+		fontSize: 18,
+		marginLeft: 10,
+		color: "#afd6ed",
+	},
+	serivicesText: {
+		fontSize: 32,
+		color: "#1779d4",
+		marginTop: 10,
+		fontWeight: "bold",
+	},
+	servicesContainer: {
+		width: "100%",
+		height: 400,
+		//backgroundColor: "blue",
+		borderRadius: 25,
+		marginVertical: 10,
+		//padding: 10,
+	},
+	servicesImage: {
+		height: "100%",
+		width: "100%",
+		//overflow: "hidden",
+	},
+	minPriceContainer: {
+		width: "25%",
+		height: "25%",
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "blue",
+		alignSelf: "flex-end",
+		position: "absolute",
+		bottom: 20,
+		right: 20,
+		borderTopLeftRadius: 40,
+		borderBottomRightRadius: 40,
+	},
+});
 
 export default styles;
